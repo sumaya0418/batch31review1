4674b50387a72a2da597b6808e9d9b3e985e19c5
Jaanku <Jaanku@DESKTOP-6TKFJQT.mynetworksettings.com>
2022-11-19 09:59:50 -0500

    this is first push to git hub

---
+++ batch31.smart_tech/src/test/java/com/smarttech/batch31/Apple.java
@@ -0,0 +1,13 @@
+package com.smarttech.batch31;
+
+public class Apple {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("go");
+		System.out.println("a");
+		System.out.println("B");
+	}
+
+}
---
+++ batch31.smart_tech/src/test/java/com/smarttech/batch31/package-info.java
@@ -0,0 +1,1 @@
+package com.smarttech.batch31;
